daf90a0673275400fe2eb2b76eee4a2b308f8bf5
Andrew Dolan <andrew.dolan@skyward.io>
2022-08-29 11:13:03 -0700

    Switch to Flask, fastAPI is unsupported by render

--- postFeedback.py
+++ postFeedback.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI
-from typing import Optional
+from flask import Flask, request
 from pydantic import BaseModel
+from typing import Optional
 import requests
 
-app = FastAPI();
+app = Flask(__name__)
 
 class Feedback(BaseModel):
 	like: bool
 	author: Optional[str]
 
-@app.post("/feedback")
-def root(feedback: Feedback):
+@app.route("/feedback", methods = ['POST'])
+def feedback():
+	# return request.get_json()
 	return requests.get("https://timeapi.io/api/Time/current/zone?timeZone=America/Los_Angeles").json()
