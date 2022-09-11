21b17f1213422311c3ea70c778b6a135fb66f098
Andrew Dolan <andrew.dolan@skyward.io>
2022-09-11 07:55:03 -0700

    Enable CORS

--- postFeedback.py
+++ postFeedback.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, Response
+from flask_docs_api.api import Api
+from flask_cors import CORS, cross_origin
 import tweeter
 import requests
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/feedback", methods = ['POST'])
+@cross_origin()
 def feedback():
 	feedback = request.get_json()
 	result = {}
